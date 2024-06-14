0fc94a81d54315902360ad85b6a6d911722b2350
jhamile08 <jhamiledr222@gmail.com>
2024-06-14 15:01:25 -0500

    added script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-
+import {URL_PRODUCT} from "../apiConnection/URL.js";
+import {get} from "../apiConnection/apiConnection.js";
 // Swipper
   var swiper = new Swiper('.slide-content', {
     navigation: {
@@ -12,8 +13,8 @@
       el: '.swiper-pagination',
       clickable: true,
     },
-  
-    
+
+
     breakpoints: {
       660: {
       slidesPerView: 1,
@@ -35,9 +36,55 @@
       slidesPerView: 4,
       spaceBetween: 40,
       },
-    } 
+    }
       });
 
 // Inyect products from BD
 
-let container = document.querySelector('.categoria')
+document.addEventListener("DOMContentLoaded",()=>{
+  renderProducts();
+})
+
+let containerProducts = document.querySelector('.category');
+
+async function renderProducts() {
+  let products = await get(URL_PRODUCT);
+
+  let containers = {
+    "CARTUCHO":document.querySelector("#cartuchos"),
+    "TONER":document.querySelector("#toner")
+  }
+  containerProducts.innerHTML = "";
+  products.content.forEach(product => {
+    let container = containers[product.typeProduct]
+    if(container){
+      containerProducts.innerHTML += `
+      <div class="category" id="${product.typeProduct}" data-category="${product.typeProduct}">
+        <div class="container-cards" data-category="${product.typeProduct}-${product.brandProduct}-${product.qualityProduct}">
+          <div class="content-card">
+              <div>
+                  <img class="img-product" src="${product.imgProduct}"
+                      alt="${product.nameProduct}">
+              </div>
+              <div>
+                  <hr>
+                  <div class="container-name-product">
+                      <h3 class="name-product"> ${product.nameProduct}</h3>
+                  </div>
+                  <hr>
+                  <p class="performance-product"><b>Rendimiento:</b> ${product.performanceProduct}</p>
+                  <p class="compatibility-product"><b>Compatibilidad:</b> ${product.compatibilityProduct}</p>
+                  <p class="price-product"><b>Price:</b> ${product.priceProduct}</p>
+                  <a class="button-buy" target="_blank"
+                      href="https://wa.me/573195806583?text=Hola, Estoy interesado en el ${product.nameProduct}"><i
+                          class="fa-brands fa-whatsapp"></i>Comprar</a>
+              </div>
+          </div>
+          </div>
+    </div>
+    `
+    }
+
+});
+}
+renderProducts()
